2d7b7e62888776f08932afd9b32895895cf4ce58
Guilherme Ytalo <gydaytalo@gmail.com>
2025-05-07 10:02:24 -0300

    feat[products]: added alembic and orms

--- alembic/env.py
+++ alembic/env.py
@@ -1,3 +1,8 @@
+import os
+from dotenv import load_dotenv
+from app.infrastructure.db.database import Base
+from app.infrastructure.db.models import *  # importa todos os modelos
+from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -5,9 +10,14 @@ from sqlalchemy import pool
 
 from alembic import context
 
+load_dotenv()  # Carrega variáveis do .env
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+# DATABASE_URL do .env
+config.set_main_option("sqlalchemy.url", os.environ["DATABASE_URL"])
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -18,7 +28,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata =  Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
